8a9bdf84039d05669f571356ac00450116b22527
eltonk2k <elieltonpb1@gmail.com>
2022-08-08 22:01:32 -0300

    Game Loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,7 +13,7 @@ public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
-	private float xDir = 0.003f, yDir = 0.003f;
+	private float xDir = 1f, yDir = 1f;
 	private int frames = 0;
 	private long lastCheck = 0;
 	private Color color = new Color(150, 20, 90);
@@ -51,14 +51,7 @@ public class GamePanel extends JPanel {
 		g.setColor(color);
 		g.fillRect((int) xDelta, (int) yDelta, 200, 50);
 
-		frames++;
-		if (System.currentTimeMillis() - lastCheck >= 1000) {
-			lastCheck = System.currentTimeMillis();
-			System.out.println("FPS: " + frames);
-			frames = 0;
-		}
-
-		repaint();
+		
 	}
 
 	private void updateRectangle() {
