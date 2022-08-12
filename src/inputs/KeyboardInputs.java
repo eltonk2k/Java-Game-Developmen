801f3dc600727363028d0eb88e628dbe713a39a3
eltonk2k <elieltonpb1@gmail.com>
2022-08-11 22:57:56 -0300

    Animations

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -5,46 +5,52 @@ import java.awt.event.KeyListener;
 
 import main.GamePanel;
 
+import static utils.Constants.Directions.*;
+
 public class KeyboardInputs implements KeyListener {
 
 	private GamePanel gamePanel;
-	
+
 	public KeyboardInputs(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 	}
-	
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
+
 		switch (e.getKeyCode()) {
-		
 		case KeyEvent.VK_W:
-			gamePanel.changeYDelta(-5);
+		case KeyEvent.VK_A:
+		case KeyEvent.VK_S:
+		case KeyEvent.VK_D:
+			gamePanel.setMoving(false);
+			break;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.setDirection(UP);
 			break;
 		case KeyEvent.VK_A:
-			gamePanel.changeXDelta(-5);
+			gamePanel.setDirection(LEFT);
 			break;
 		case KeyEvent.VK_S:
-			gamePanel.changeYDelta(5);
+			gamePanel.setDirection(DOWN);
 			break;
 		case KeyEvent.VK_D:
-			gamePanel.changeXDelta(5);
+			gamePanel.setDirection(RIGHT);
 			break;
-			
-			
 		}
+
 	}
 
 }
