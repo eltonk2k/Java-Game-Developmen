73e4246156cf5ba79179ce1ab817b275d0a30683
eltonk2k <elieltonpb1@gmail.com>
2022-09-20 22:32:44 -0300

    Player Class

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.awt.Graphics;
+
+import entities.Player;
+
 public class Game implements Runnable {
 	
 	private GameWindow gameWindow;
@@ -8,12 +12,19 @@ public class Game implements Runnable {
 	private final int FPS_SET = 120;
 	private final int UPS_SET = 200;
 	
+	private Player player;
 	
 	public Game() {
-		gamePanel = new GamePanel();
+		initClasses();
+		gamePanel = new GamePanel(this);
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();
 		startGameLoop();
+		
+	}
+
+	private void initClasses() {
+		player = new Player(200, 200);
 	}
 
 	private void startGameLoop() {
@@ -22,7 +33,11 @@ public class Game implements Runnable {
 	}
 
 	public void update() {
-		gamePanel.updateGame();
+		player.update();
+	}
+	
+	public void render(Graphics g) {
+		player.render(g);
 	}
 	
 	@Override
@@ -60,8 +75,20 @@ public class Game implements Runnable {
 				updates = 0;
 			}
 		}
+	}
+	
+	
+	public void windowLostFocus() {
+		player.resetDirBooleans();
+		
 		
 	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+
+	
 
 	
 }
---
+++ src/utils/LoadSave.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class LoadSave {
+	
+	public static final String PLAYER_ATLAS = "player_sprites.png";
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+		try {
+			img = ImageIO.read(is);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}
+}
